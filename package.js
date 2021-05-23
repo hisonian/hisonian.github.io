8aa64a24907eb150e75eb28d42bf1bab07c1641f
hisonian <82769734+hisonian@users.noreply.github.com>
2021-05-23 22:35:01 +0300

    upload site

---
+++ package.js
@@ -0,0 +1,28 @@
+// prelodaer
+
+window.onload = function () {
+   document.body.classList.add('loaded_hiding');
+   window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 500);
+}
+
+// scroll on top
+
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
